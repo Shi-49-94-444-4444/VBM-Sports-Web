26d2732a509ae652831f2c145ce38afa1a055c83
Shi <langtusayonly@gmail.com>
2023-11-11 18:21:16 +0700

    test deploy #4

--- pages/product/list-badminton.tsx
+++ pages/product/list-badminton.tsx
@@ -1,29 +1,29 @@
-// import { listItems } from "@/utils";
-// import Layout from '@/app/layout';
-// import {
-//     Container,
-//     ProductFilter,
-//     ProductItems
-// } from "@/app/components";
+import { listItems } from "@/utils";
+import Layout from '@/app/layout';
+import {
+    Container,
+    // ProductFilter,
+    // ProductItems
+} from "@/app/components";
 
-// const ListBadmintonPage = () => {
-//     return (
-//         <Layout>
-//             <Container>
-//                 <div className="py-10">
-//                     <div className="grid lg:grid-cols-4 col-span-1 gap-5">
-//                         <div className="lg:col-span-1 flex flex-col gap-3">
-//                             <ProductFilter />
-//                         </div>
-//                         <div className="lg:col-span-3 col-span-1 h-auto w-full relative">
-//                             <h1 className="text-3xl font-semibold pb-5 text-gray-600">Bài đăng đặt sân</h1>
-//                             <ProductItems listItem={listItems} />
-//                         </div>
-//                     </div>
-//                 </div>
-//             </Container>
-//         </Layout>
-//     )
-// }
+const ListBadmintonPage = () => {
+    return (
+        <Layout>
+            <Container>
+                <div className="py-10">
+                    {/* <div className="grid lg:grid-cols-4 col-span-1 gap-5">
+                        <div className="lg:col-span-1 flex flex-col gap-3">
+                            <ProductFilter />
+                        </div>
+                        <div className="lg:col-span-3 col-span-1 h-auto w-full relative">
+                            <h1 className="text-3xl font-semibold pb-5 text-gray-600">Bài đăng đặt sân</h1>
+                            <ProductItems listItem={listItems} />
+                        </div>
+                    </div> */}
+                </div>
+            </Container>
+        </Layout>
+    )
+}
 
-// export default ListBadmintonPage
+export default ListBadmintonPage
