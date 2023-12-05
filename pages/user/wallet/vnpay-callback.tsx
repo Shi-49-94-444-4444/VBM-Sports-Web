a01030d5606194458f9ebef26c9ec2b457250907
Shi <langtusayonly@gmail.com>
2023-12-05 22:17:14 +0700

    Fix recharge wallet

--- pages/user/wallet/vnpay-callback.tsx
+++ pages/user/wallet/vnpay-callback.tsx
@@ -12,17 +12,20 @@ export default function VnPayCallback() {
     const { vnp_ResponseCode, vnp_TransactionStatus, vnp_Amount } = router.query
 
     useEffect(() => {
-        if (vnp_ResponseCode && vnp_TransactionStatus && vnp_Amount && user && user.id) {
+        if (vnp_ResponseCode && vnp_TransactionStatus && vnp_Amount) {
             if (vnp_ResponseCode == "00" && vnp_TransactionStatus == "00") {
-                walletService({ id: user.id, money: Number(vnp_Amount.toString()) })
-                router.push('/user/wallet/wallet-success')
-                window.opener.postMessage('payment completed', '*')
+                (async () => {
+                    if (user && user.id) await walletService({ id: user.id, money: Number(vnp_Amount.toString())/100 })
+                    router.push('/user/wallet/wallet-success')
+                    window.opener.postMessage('payment completed', '*')
+                })();
             } else {
                 router.push('/user/wallet/wallet-fail')
                 window.opener.postMessage('payment completed', '*')
             }
         }
     }, [router, vnp_ResponseCode, vnp_TransactionStatus, vnp_Amount, user])
+
 
     return (
         <div className="h-screen flex items-center justify-center">
