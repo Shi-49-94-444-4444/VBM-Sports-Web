47b9e07aedbba55dd34a717b152191bc343d7f98
Shi <langtusayonly@gmail.com>
2024-06-10 14:35:14 +0700

    upgrade cross

--- services/AxiosInstance.ts
+++ services/AxiosInstance.ts
@@ -1,6 +1,10 @@
 import axios from 'axios';
+import https from 'https'
 
 const AxiosClient = axios.create({
+  httpsAgent: new https.Agent({
+    rejectUnauthorized: false
+  }),
   baseURL: process.env.API_BASE_URL,
   headers: {
     'Content-Type': 'application/json',
